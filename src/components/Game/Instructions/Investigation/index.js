72f90a29c086d7351895b5cb88f38cf618a9cf1f
Julien Verkest <julienverkest@gmail.com>
2019-04-24 16:39:43 +0200

    change README.md

--- src/components/Game/Instructions/Investigation/index.js
+++ src/components/Game/Instructions/Investigation/index.js
@@ -7,53 +7,53 @@ class Investigation extends Component {
     this.props.setScroll();
   }
   render() {
-    const { setScroll, step, onChangeStep, user, userRank, isSound, Cases, lang, userCase, Config } = this.props;
+    const { setScroll, step, onChangeStep, user, userRank, Cases, lang, userCase, Config } = this.props;
     return (
       <div>
-        <Typemachine mb={'mb-4'} setScroll={setScroll} mystring={Cases[userCase][lang].start[0]} step={step} changeStep={onChangeStep} isSound={isSound}> </Typemachine>
+        <Typemachine mb={'mb-4'} setScroll={setScroll} mystring={Cases[userCase][lang].start[0]} step={step} changeStep={onChangeStep} > </Typemachine>
         {
           step > 10
-          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={Cases[userCase][lang].start[1]} step={step} changeStep={onChangeStep} isSound={isSound}> </Typemachine>
+          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={Cases[userCase][lang].start[1]} step={step} changeStep={onChangeStep} > </Typemachine>
           : ''
         }
         {
           step > 11
-          ? <Typemachine mb={'mb-4'} setScroll={setScroll} mystring={Cases[userCase][lang].start[2]}   step={step} changeStep={onChangeStep} isSound={isSound} > </Typemachine>
+          ? <Typemachine mb={'mb-4'} setScroll={setScroll} mystring={Cases[userCase][lang].start[2]}   step={step} changeStep={onChangeStep}  > </Typemachine>
           : ''
         }
         {
           step > 12
-          ? <ButtonNext mb={'mb-4'} step={step}  setScroll={setScroll} onChangeStep={onChangeStep} nextStep={14} isSound={isSound}>{Config[lang].buttons.continue} </ButtonNext>
+          ? <ButtonNext mb={'mb-4'} step={step}  setScroll={setScroll} onChangeStep={onChangeStep} nextStep={14} >{Config[lang].buttons.continue} </ButtonNext>
           : ''
         }
         {
           step > 13
-          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={Cases[userCase][lang].start[3]} step={step} changeStep={onChangeStep} isSound={isSound}> </Typemachine>
+          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={Cases[userCase][lang].start[3]} step={step} changeStep={onChangeStep} > </Typemachine>
           : ''
         }
         {
           step > 14
-          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={Cases[userCase][lang].start[4]}  step={step} changeStep={onChangeStep} isSound={isSound}> </Typemachine>
+          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={Cases[userCase][lang].start[4]}  step={step} changeStep={onChangeStep} > </Typemachine>
           : ''
         }
         {
           step > 15
-          ? <ButtonNext mb={'mb-4'} step={step}  setScroll={setScroll} onChangeStep={onChangeStep} nextStep={17} isSound={isSound}>{Config[lang].buttons.continue} </ButtonNext>
+          ? <ButtonNext mb={'mb-4'} step={step}  setScroll={setScroll} onChangeStep={onChangeStep} nextStep={17} >{Config[lang].buttons.continue} </ButtonNext>
           : ''
         }
         {
           step > 16
-          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={Cases[userCase][lang].start[5]} step={step} changeStep={onChangeStep} isSound={isSound}> </Typemachine>
+          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={Cases[userCase][lang].start[5]} step={step} changeStep={onChangeStep} > </Typemachine>
           : ''
         }
         {
         step > 17
-          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={`${Cases[userCase][lang].start[6]} ${userRank} ${user} `}  step={step} changeStep={onChangeStep} isSound={isSound}> </Typemachine>
+          ? <Typemachine mb={'mb-8'} setScroll={setScroll} mystring={`${Cases[userCase][lang].start[6]} ${userRank} ${user} `}  step={step} changeStep={onChangeStep} > </Typemachine>
           : ''
         }
         {
           step > 18
-          ? <ButtonNext mb={'mb-4'} step={step} setScroll={setScroll} onChangeStep={onChangeStep} nextStep={20} isSound={isSound}>{Config[lang].buttons.game} </ButtonNext>
+          ? <ButtonNext mb={'mb-4'} step={step} setScroll={setScroll} onChangeStep={onChangeStep} nextStep={20} >{Config[lang].buttons.game} </ButtonNext>
           : ''
         }
       </div>
